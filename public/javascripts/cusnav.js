358b3bf455b0e94786ee522ed3830283b1e32b21
Changbeom Choi <me@cbchoi.info>
2021-04-20 08:27:45 +0900

    update test

--- public/javascripts/cusnav.js
+++ public/javascripts/cusnav.js
@@ -6,19 +6,32 @@
  */
 AFRAME.registerComponent('navigate', {
     schema: {
-        on: {type: 'string'},
         url: {type: 'string'},
     },
 
+/*
     init: function () {
         var data = this.data;
         var el = this.el;
 
-        el.addEventListener(data.on, function (event) {
+        this.el.addEventListener('click', function (event) {
             event.preventDefault();
             // Fade out image.
             window.location = data.url;
             // Wait for fade to complete.
         });
-    },
+    },*/
+    init: function () {
+    var lastIndex = -1;
+    var COLORS = ['red', 'green', 'blue'];
+    this.el.addEventListener('click', function (evt) {
+      var el1 = document.querySelectorAll('a-link');
+      for(elem of el1)
+        console.log(elem.getAttribute('position'));
+
+      lastIndex = (lastIndex + 1) % COLORS.length;
+      this.setAttribute('material', 'color', COLORS[lastIndex]);
+      console.log('I was clicked at: ', evt.detail.intersection.point);
+    });
+  }
 });
